f13ffb7dc00b6c6b7a9bf199d098890ce060fa35
Vinicius Alvarenga <valvarenga@impresa.pt>
2016-02-11 10:07:37 +0000

    indentation

--- norm-transaction/src/main/java/org/irenical/norm/transaction/NormCall.java
+++ norm-transaction/src/main/java/org/irenical/norm/transaction/NormCall.java
@@ -6,17 +6,17 @@ import java.sql.SQLException;
 
 public class NormCall <INPUT, OUTPUT> extends NormOperation<INPUT, OUTPUT> {
 
-    @Override
-    OUTPUT execute(NormContext<INPUT, OUTPUT> context) throws SQLException {
-        try (PreparedStatement statement = JDBChops.prepareStatementForCall(context.getConnection(), queryBuilder.apply(context), parametersBuilder == null ? null : parametersBuilder.apply(context))) {
-            ResultSet rs = statement.executeQuery();
-            context.setPreparedStatement(statement);
-            context.setResultSet(rs);
-            if (outputReader != null) {
-                return outputReader.toOutput(context);
-            }
-            return null;
-        }
+  @Override
+  OUTPUT execute(NormContext<INPUT, OUTPUT> context) throws SQLException {
+    try (PreparedStatement statement = JDBChops.prepareStatementForCall(context.getConnection(), queryBuilder.apply(context), parametersBuilder == null ? null : parametersBuilder.apply(context))) {
+      ResultSet rs = statement.executeQuery();
+      context.setPreparedStatement(statement);
+      context.setResultSet(rs);
+      if (outputReader != null) {
+        return outputReader.toOutput(context);
+      }
+      return null;
     }
+  }
 
 }
