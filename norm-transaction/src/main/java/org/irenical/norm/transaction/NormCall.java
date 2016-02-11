ad29650d94582e0b00b4345cb9c5303f2cf7608c
Vinicius Alvarenga <valvarenga@impresa.pt>
2016-02-11 09:56:05 +0000

    fixing NormCall: now using executeQuery instead of executeUpdate

--- norm-transaction/src/main/java/org/irenical/norm/transaction/NormCall.java
+++ norm-transaction/src/main/java/org/irenical/norm/transaction/NormCall.java
@@ -1,21 +1,22 @@
 package org.irenical.norm.transaction;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class NormCall<INPUT, OUTPUT> extends NormOperation<INPUT, OUTPUT> {
+public class NormCall <INPUT, OUTPUT> extends NormOperation<INPUT, OUTPUT> {
 
-  @Override
-  OUTPUT execute(NormContext<INPUT, OUTPUT> context) throws SQLException {
-    try (PreparedStatement statement = JDBChops.prepareStatementForCall(context.getConnection(), queryBuilder.apply(context), parametersBuilder == null ? null : parametersBuilder.apply(context))) {
-      int count = statement.executeUpdate();
-      context.setPreparedStatement(statement);
-      context.setUpdatedRows(count);
-      if (outputReader != null) {
-        return outputReader.toOutput(context);
-      }
-      return null;
+    @Override
+    OUTPUT execute(NormContext<INPUT, OUTPUT> context) throws SQLException {
+        try (PreparedStatement statement = JDBChops.prepareStatementForCall(context.getConnection(), queryBuilder.apply(context), parametersBuilder == null ? null : parametersBuilder.apply(context))) {
+            ResultSet rs = statement.executeQuery();
+            context.setPreparedStatement(statement);
+            context.setResultSet(rs);
+            if (outputReader != null) {
+                return outputReader.toOutput(context);
+            }
+            return null;
+        }
     }
-  }
 
 }
