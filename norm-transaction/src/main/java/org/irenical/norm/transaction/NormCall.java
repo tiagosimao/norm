a6e23a0306ca8a4f18cbfe4da7383db27e9cfbab
Diogo Neves <dsneves@impresa.pt>
2016-03-17 22:07:35 +0000

    fix stored procedure calls

--- norm-transaction/src/main/java/org/irenical/norm/transaction/NormCall.java
+++ norm-transaction/src/main/java/org/irenical/norm/transaction/NormCall.java
@@ -1,17 +1,23 @@
 package org.irenical.norm.transaction;
 
+import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class NormCall <INPUT, OUTPUT> extends NormOperation<INPUT, OUTPUT> {
+public class NormCall<INPUT, OUTPUT> extends NormOperation<INPUT, OUTPUT> {
 
   @Override
   OUTPUT execute(NormContext<INPUT, OUTPUT> context) throws SQLException {
-    try (PreparedStatement statement = JDBChops.prepareStatementForCall(context.getConnection(), queryBuilder.apply(context), parametersBuilder == null ? null : parametersBuilder.apply(context))) {
-      ResultSet rs = statement.executeQuery();
-      context.setPreparedStatement(statement);
-      context.setResultSet(rs);
+    try (CallableStatement statement = JDBChops.prepareStatementForCall(context.getConnection(),
+            queryBuilder.apply(context),
+            parametersBuilder == null ? null : parametersBuilder.apply(context))
+    ) {
+      boolean hasResultSet = statement.execute();
+
+      context.setCallableStatement(statement);
+      context.setResultSet(hasResultSet ? statement.getResultSet() : null);
+
       if (outputReader != null) {
         return outputReader.toOutput(context);
       }
